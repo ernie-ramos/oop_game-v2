05d51832c86bc0c2577883446aafe356ba9dadd5
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2020-06-30 15:49:44 -0400

    Completed Step 6
    Step 6 in the study guide.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,38 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = this.createPhrases();
+    this.activePhrase = null;
+  }
+
+  /**
+   * Creates phrases for use in game
+   * @return {array} An array of phrases that could be used in the game
+   */
+  createPhrases() {
+    const a = 'all over the map';
+    const b = 'before you know it';
+    const c = 'blow off steam';
+    const d = 'break a leg';
+    const e = 'call it a day';
+    return [
+      new Phrase(a),
+      new Phrase(b),
+      new Phrase(c),
+      new Phrase(d),
+      new Phrase(e),
+    ];
+  }
+
+  /**
+   * Selects random phrase from phrases property
+   * @return {Object} Phrase object chosen to be used
+   */
+  getRandomPhrase() {
+    return this.phrases[Math.floor(Math.random() * 5)];
+    //Math.floor(Math.random() * 5 )
+  }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,59 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+
+  /**
+   * Display phrase on game board
+   */
+  addPhraseToDisplay() {
+    const ul = document.querySelector('.main-container ul');
+
+    const game = new Game();
+    const phrase = game.getRandomPhrase();
+    const indPhrase = phrase.phrase;
+
+    let phraseArr = [];
+    for (let i = 0; i < indPhrase.length; i++) {
+      phraseArr.push(indPhrase[i]);
+    }
+
+    phraseArr.forEach((char) => {
+      const isAlpha = (ch) => {
+        return /^[A-Z]$/i.test(ch);
+      }; // https://stackoverflow.com/questions/40120915/javascript-function-that-returns-true-if-a-letter
+
+      const createLi = (string) => {
+        const li = document.createElement('li');
+        li.className = string;
+        li.textContent = char;
+        ul.appendChild(li);
+      };
+
+      isAlpha(char) ? createLi(`hide letter ${char}`) : createLi('space');
+    });
+  }
+}
+
+// all over the map
+// before you know it
+// blow off steam
+// break a leg
+// call it a day
+// cat nap
+// disco nap
+// get the hang of it
+// give props to
+// head over heels
+// here and there
+// hit me up
+// keep me in the loop
+// keep me posted
+// not for nothing
+// ruffle feathers
+// sea legs
+// take the wind out of your sails
+// two peas in a pod
